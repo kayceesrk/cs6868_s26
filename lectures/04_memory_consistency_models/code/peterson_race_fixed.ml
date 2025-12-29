d6fe2c12bacd261fdb82786424c8e6674f7623e2
KC Sivaramakrishnan <kc@kcsrk.info>
2025-12-29 13:00:23 +0530

    Remove inline comments for cleaner slide presentation

--- lectures/04_memory_consistency_models/code/peterson_race_fixed.ml
+++ lectures/04_memory_consistency_models/code/peterson_race_fixed.ml
@@ -16,40 +16,30 @@
  *)
 
 module Peterson = struct
-  (* Atomic locations for flags - one per domain *)
   let flag = [| Atomic.make false; Atomic.make false |]
-  
-  (* Atomic location for the victim variable *)
   let victim = Atomic.make 0
 
   let lock () =
     let i = (Domain.self () :> int) - 1 in
     let j = 1 - i in
-    (* Set our flag atomically - signals we want to enter critical section *)
     Atomic.set flag.(i) true;
-    (* Set victim atomically - establishes happens-before with other domain *)
     Atomic.set victim i;
-    (* Busy-wait using atomic reads - no data race since all accesses are atomic *)
-    (* The atomic operations create proper synchronization between domains *)
     while Atomic.get flag.(j) && Atomic.get victim = i do
       ()
     done
 
   let unlock () =
     let i = (Domain.self () :> int) - 1 in
-    (* Atomically clear our flag - releases the lock *)
     Atomic.set flag.(i) false
 end
 
-(* Counter is still a regular ref since it's protected by Peterson's lock *)
-(* Only one domain accesses it at a time due to mutual exclusion *)
 let counter = ref 0
 
 let thread_work () =
   for _ = 1 to 10000 do
-    Peterson.lock ();        (* Acquire lock using atomic operations *)
-    incr counter;           (* Critical section - protected by the lock *)
-    Peterson.unlock ()      (* Release lock using atomic operations *)
+    Peterson.lock ();
+    incr counter;
+    Peterson.unlock ()
   done
 
 let () =
